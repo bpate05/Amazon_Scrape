e395b535a73d9b272ca8bdc077832f2b13baecaa
Brian Pate <brianwilliampate@gmail.com>
2019-06-30 22:10:31 -0400

    :bug: Fixed URLs for GoPro spider

--- amazon_scrape/amazon_scrape/spiders/GoPro.py
+++ amazon_scrape/amazon_scrape/spiders/GoPro.py
@@ -4,8 +4,8 @@ import scrapy
 
 class GoproSpider(scrapy.Spider):
     name = 'GoPro'
-    allowed_domains = ['https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/dp/B0792MJLNM/ref=sr_1_3?crid=D3C7EDM435E7']
-    start_urls = ['http://https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/dp/B0792MJLNM/ref=sr_1_3?crid=D3C7EDM435E7/']
+    allowed_domains = ['www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/dp/B0792MJLNM/ref=sr_1_3?crid=D3C7EDM435E7']
+    start_urls = ['http://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/dp/B0792MJLNM/ref=sr_1_3?crid=D3C7EDM435E7/']
 
     def parse(self, response):
         pass
