b523642114b3320c5c1deffe2e72c4bffb268803
Brian Pate <brianwilliampate@gmail.com>
2019-07-03 19:55:30 -0400

    :art: Format improved and comments added

--- amazon_scrape/amazon_scrape/pipelines.py
+++ amazon_scrape/amazon_scrape/pipelines.py
@@ -1,3 +1,4 @@
+"""Builds pipeline to MongoDB"""
 import pymongo
 
 class AmazonScrapePipeline(object):
@@ -10,22 +11,21 @@ class AmazonScrapePipeline(object):
 
     @classmethod
     def from_crawler(cls, crawler):
+        """Store variables to paths for local MongoDB and name DB"""
         return cls(
             mongo_uri=crawler.settings.get('mongodb://localhost:27017/mydb'),
             mongo_db=crawler.settings.get('Amazon_DB', 'Amazon')
         )
 
     def open_spider(self, spider):
+        """Opens spider and establishes connections to DB"""
         self.client = pymongo.MongoClient(self.mongo_uri)
         self.db = self.client[self.mongo_db]
 
     def close_spider(self, spider):
         self.client.close()
 
-    # def process_item(self, item, spider):
-    #     self.db[self.collection_name].insert_one(dict(item))
-    #     return item
-
     def process_item(self, item, spider):
+        """Insert items into collection"""
         self.db[self.collection_name].insert_one(item)
         return item
--- amazon_scrape/amazon_scrape/spiders/GoPro.py
+++ amazon_scrape/amazon_scrape/spiders/GoPro.py
@@ -1,28 +1,37 @@
 import scrapy
+"""Define spiders for GoPro"""
+
 from amazon_scrape.items import AmazonScrapeItem
 
 class GoproSpider(scrapy.Spider):
+    """GoPro Spider class"""
     name = 'GoProReviews'
+    # indicates the next page number to be scraped
     page_number = 2
     allowed_domains = ['www.amazon.com']
     start_urls = ['https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/product-reviews/B0792MJLNM/ref=cm_cr_dp_d_show_all_top?ie=UTF8&reviewerType=all_reviews&pageNumber=1']
 
     def parse(self, response):
+        """Obtains identified objects from start_url"""
         items = AmazonScrapeItem()
 
+        # paths to each of the elements to be scraped
         ids = response.xpath("//div[@data-hook='review']/@id").getall()
         titles = response.xpath("//a[@class='a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold']/span/text()").getall()
         dates = response.xpath("//span[@data-hook='review-date']/text()").getall()
         stars = response.xpath("//i[@data-hook='review-star-rating']/span[@class='a-icon-alt']/text()").getall()
         text = response.xpath("//span[@data-hook='review-body']/span/text()").getall()
 
+        # from items.py
         items['review_id'] = ids
         items['title'] = titles
         items['date'] = dates
         items['stars'] = stars
         items['text'] = text            
         
-        for ids, title, date, star, text in zip(items['review_id'], items['title'], items['date'], items['stars'], items['text']):
+        # zips content together and yields a dictionary
+        for ids, title, date, star, text in zip(items['review_id'], items['title'], 
+            items['date'], items['stars'], items['text']):
             yield {
                 'review_id': ids.strip(),
                 'title': title.strip(),
@@ -31,11 +40,12 @@ class GoproSpider(scrapy.Spider):
                 'text': text.strip()
             }
         
+        # next page to be scraped
         next_page = 'https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/product-reviews/B0792MJLNM/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber=' + str(GoproSpider.page_number)
+        
+        # loop through all pages of reviews
         if GoproSpider.page_number <= 8:
+            # increase page number
             GoproSpider.page_number += 1
-            yield response.follow(next_page, callback = self.parse) 
-
-        # next_relative_url = 'response.xpath("//li[@class='a-last']//@href").get()'
-        # next_abs_url = response.urljoin(next_relative_url)
-
+            # call back to parse, store elements on new page
+            yield response.follow(next_page, callback = self.parse)
